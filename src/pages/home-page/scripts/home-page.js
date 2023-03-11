bf296995c6001680179d96dada0c1aeb38a21c4b
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-11 22:21:12 +0100

    Project done

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -6,7 +6,33 @@ import "../styles/section.scss";
 import "../styles/summary-box.scss";
 import "../styles/summary-text.scss";
 import "../styles/summary-plan.scss";
+import "../styles/loader.scss";
 
-import { setupCounter } from "../../../global-scripts/scripts/counter/counter.js";
+const btn = document.getElementById("proceed-btn");
+const loader = document.getElementById("loader");
+const percent = document.getElementById("percent");
+const progress = document.getElementById("progress");
 
-setupCounter(document.querySelector("#counter"));
+let count = 4;
+let per = 8;
+let loading;
+
+const animate = () => {
+  if (count === 100) {
+    clearInterval(loading);
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  } else {
+    per += 3;
+    count += 1;
+    progress.style.width = per + "px";
+    percent.textContent = count + "%";
+  }
+};
+
+btn.addEventListener("click", () => {
+  btn.classList.add("c-summary-box__payment-btn--hide");
+  loader.classList.add("c-loader--active");
+  loading = setInterval(animate, 20);
+});
